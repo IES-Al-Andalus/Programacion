e5a42ef3dd284997a3da013f5a117b7a64988662
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2017-12-01 22:01:22 +0100

    Modifico el estilo para colapsar los ejercicios.

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,43 @@ $(document).ready(function(){
     event.preventDefault();
   });
 
-$( ".boton-menu" ).click(function() {
-  $( "nav" ).slideToggle( "slow", function() {
+  $("h2[id='ejercicios'] ~ ul > li").addClass("oculto")
+  $("h2[id='ejercicios'] ~ ul li ul").hide();
+  //$("h2[id='ejercicios'] + ul li p").text($("h2[id='ejercicios'] + ul li p").text() + " [ + ]");
+  //$("h2[id='ejercicios'] + ul li p:first-child").append("<span> [ + ]</span>");
+  $("h2[id='ejercicios'] ~ ul li").click(function() {
+    if (!$(this).hasClass("visible")) {
+      var nodo_visible = $(this).parent().parent().find("li.visible");
+      nodo_visible.find("ul").slideToggle();
+      nodo_visible.removeClass("visible").addClass("oculto");
+      $(this).find("ul").slideToggle();
+      $(this).addClass("visible").removeClass("oculto");
+    } else {
+      $(this).find("ul").slideToggle();
+      $(this).removeClass("visible").addClass("oculto");
+    }
+    /*var nodo = $(this).find("ul");
+    var html = $(this).find("p:first").html();
+    alert(html);
+    $(this).find("p:first").html(html.replace(" [ - ]", " [ + ]"));
+    if (!nodo.hasClass("visible")) {
+      $(this).html(html.replace(" [ - ]", " [ + ]"));
+      var html = nodo.parent().parent().find("ul.visible").parent().find("p:first").html();
+      if (html != null) {
+        //alert(html);
+        nodo.parent().parent().find("ul.visible").parent().find("p:first").html(html.replace('+', '-'));
+      }
+      nodo.parent().parent().find("ul.visible").slideToggle();
+      nodo.parent().parent().find("ul.visible").removeClass("visible").addClass("oculto");
+      nodo.slideToggle();
+      nodo.addClass("visible").removeClass("oculto");
+    } else {
+      var html = nodo.parent().parent().find("ul.visible").parent().find("p:first").html();
+      //alert(html);
+      nodo.parent().parent().find("ul.visible").parent().find("p:first").html(html.replace('-', '+'));
+      nodo.slideToggle();
+      nodo.removeClass("visible").addClass("oculto");
+    }*/
   });
-});
-//$( "section").hide();
-//mostrarSeccion("introduccion");
+
 });
